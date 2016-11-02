62030acf31638c9cc9cdb6e28fe7cce761bb4444
“JPUENTE” <jpuente@vortexbird.com>
2016-11-01 20:06:02 -0500

    test add view controller

---
+++ SofibMobile/SofibMobile/ConsultaSolicitudesAtencionController.swift
@@ -0,0 +1,35 @@
+//
+//  ConsultaSolicitudesAtencionController.swift
+//  SofibMobile
+//
+//  Created by JUAN CAMILO PUENTE RIASCOS on 11/1/16.
+//  Copyright © 2016 vortexbird. All rights reserved.
+//
+
+import UIKit
+
+class ConsultaSolicitudesAtencionController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
