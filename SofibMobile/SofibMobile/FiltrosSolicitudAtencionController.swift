f8cbd0ba069642c76526eab3ae3d897bcc0946b4
Cesar Lopez <clopez@vortexbird.com>
2016-11-15 23:50:29 -0500

    top bar modificado

--- SofibMobile/SofibMobile/FiltrosSolicitudAtencionController.swift
+++ SofibMobile/SofibMobile/FiltrosSolicitudAtencionController.swift
@@ -18,6 +18,7 @@ class FiltrosSolicitudAtencionController: UIViewController, UIPickerViewDelegate
     @IBOutlet var lblBusquedaAvanzada: UILabel!
     
     
+    @IBOutlet var navigationBar: UINavigationBar!
     @IBOutlet var txtIdentificacion: UITextField!
     @IBOutlet var txtSolicitudAtencion: UITextField!
     @IBOutlet var txtNombre: UITextField!
@@ -30,6 +31,10 @@ class FiltrosSolicitudAtencionController: UIViewController, UIPickerViewDelegate
     
     
     override func viewWillAppear(_ animated: Bool) {
+        
+        
+      
+        
         
         lblBusquedaAvanzada.text = NSLocalizedString("busqueda_avanzada", comment: "Busqueda Avanzada")
         txtIdentificacion.placeholder = NSLocalizedString("identificacion", comment: "Identificacion")
