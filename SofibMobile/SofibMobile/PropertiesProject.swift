7e7a8dd0f3388a63e91a5ae3dcba0577bfab9b64
“JPUENTE” <jpuente@vortexbird.com>
2016-11-28 19:17:55 -0500

    merge

---
+++ SofibMobile/SofibMobile/PropertiesProject.swift
@@ -0,0 +1,15 @@
+//
+//  PropertiesProject.swift
+//  SofibMobile
+//
+//  Created by JUAN CAMILO PUENTE RIASCOS on 11/26/16.
+//  Copyright © 2016 vortexbird. All rights reserved.
+//
+
+import Foundation
+
+class PropertiesProject{
+    
+    static var URL: String = "http://pruebas-sectorsalud.coomeva.com.co/saludmp-ws/jax-rs/saludmp-sofibmobile/"
+
+}
