068a64bda40d0f4ac3f7347fc9915b571a4dc3f3
“JPUENTE” <jpuente@vortexbird.com>
2016-12-12 20:34:34 -0500

    consulta con filtros y login

---
+++ SofibMobile/SofibMobile/Swift3Base64.swift
@@ -0,0 +1,9 @@
+//
+//  Swift3Base64.swift
+//  SofibMobile
+//
+//  Created by JUAN CAMILO PUENTE RIASCOS on 12/7/16.
+//  Copyright © 2016 vortexbird. All rights reserved.
+//
+
+import Foundation
